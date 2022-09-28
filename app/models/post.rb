c886e2188798f8a7ebe83f66332f8681bb30b79a
verah <verahnyagaka101@gmail.com>
2022-09-28 07:46:47 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    CATEGORIES = ["Fiction","Non-Fiction"]
+    validates :title, presence: true
+    validates :category, inclusion:{
+        in: CATEGORIES,
+        # message: `must be one of: #{CATEGORIES.join(',')}`
+    }
+    validates :content, length: { minimum: 100 }
 end
